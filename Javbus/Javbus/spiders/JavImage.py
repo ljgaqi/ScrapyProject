42bcc15c83d03b94f950892684e73803332524c5
ljgaqi <liaqi@163.com>
2020-11-20 15:10:48 +0800

    新建一个对Javbus进行抓取的爬虫

--- Javbus/Javbus/spiders/JavImage.py
+++ Javbus/Javbus/spiders/JavImage.py
@@ -1,5 +1,7 @@
+import sys
+sys.path.append('..')
 import scrapy
-
+from Javbus.Javbus.items import JavbusItem
 
 class JavimageSpider(scrapy.Spider):
     name = 'JavImage'
@@ -7,4 +9,41 @@ class JavimageSpider(scrapy.Spider):
     start_urls = ['https://www.fanbus.in/']
 
     def parse(self, response):
-        pass
+        for avweb in response.xpath('//*[@id="waterfall"]/div'):
+            item = JavbusItem
+            av_url = avweb.xpath('./a/@href').extract_first()
+            av_img = avweb.xpath('./a/div/img/@src').extract_first()
+            av_title = avweb.xpath('./a/div/img/@title').extract_first()
+            av_sn = avweb.xpath('./a/div[2]/span/date[1]/text()').extract_first()
+            av_date = avweb.xpath('./a/div[2]/span/date[2]/text()').extract_first()
+            if av_url != None:  # 判断爬下来的内容不为空，再继续...
+                item['av_url'] = av_url
+                item['av_date'] = av_date
+                item['av_image_small'] = av_img
+                item['av_sn'] = av_sn
+                item['av_title'] = av_title
+
+                print('this is tyr')
+                print(av_url)
+                print(item['av_url'])
+                print({'item': item})
+
+                yield scrapy.Request(url=item['av_url'], meta={'item': item}, callback=self.parse_deep)
+                # meta参数是为了传参存在的，为了是把item传递给下一个函数。
+        # next_url = response.xpath('//*[@id="next"]/@href').extract_first()
+        # if next_url:
+        #     next_url = response.urljoin(next_url)
+        #     yield scrapy.Request(next_url, callback=self.parse)
+
+    def parse_deep(self, response):
+        item = response.meta['item']  # 我被这个中括号折磨致死，以后一定要记住[]是字典，{}不是
+        item['av_image_big'] = response.xpath('//*[@class="bigImage"]/img/@src').extract_first()
+        yield item
+
+        # yield {
+        #     'av_sn': av_sn,
+        #     'av_date': av_date,
+        #     'av_url':av_url,
+        #     'av_img':av_img,
+        #     'av_title':av_title
+        # }
